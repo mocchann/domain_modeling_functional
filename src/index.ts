748907e5529d51be2c6d1b3a9993db8c1a6be6b1
もっちゃん <s7056i3056@gmail.com>
2024-07-07 15:00:00 +0900

    add: square関数を追加

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,9 @@ const add1 = (x: number): number => x + 1;
 let y: number;
 
 const add = (x: number, y: number): number => x + y;
+
+// squarePlusOne : int -> int
+let squarePlusOne = (x: number): number => {
+  let square = x * x;
+  return square + 1;
+};
