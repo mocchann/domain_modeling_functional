3661d36ddc04ba079a37bf196103f58b75a1486a
もっちゃん <s7056i3056@gmail.com>
2024-09-07 13:15:52 +0900

    add: 実装に必要な型を作成

--- simpleTypes.ts
+++ simpleTypes.ts
@@ -4,7 +4,12 @@ type GizmoCode = { type: "gizmoCode"; gizmoCode: string };
 // 制約: 先頭が"G" + 数字4桁
 export type ProductCode = WidgetCode | GizmoCode;
 
-export type UnvalidatedCustomerInfo = undefined;
+export type UnvalidatedCustomerInfo = {
+  firstName: FirstName;
+  middleInitial?: MiddleInitial;
+  lastName: LastName;
+  emailAddress: EmailAddress;
+};
 export type UnvalidatedShippingAddress = undefined;
 export type UnvalidatedBillingAddress = undefined;
 export type UnvalidatedOrderLine = undefined;
@@ -14,7 +19,20 @@ export type OrderId = {
   readonly value: string;
 };
 export type Address = undefined;
-export type CustomerInfo = undefined;
+export type CustomerInfo = {
+  name: PersonalName;
+  emailAddress: EmailAddress;
+};
 export type ValidatedOrderLine = undefined;
 
 export type Price = undefined;
+
+export type FirstName = string;
+export type MiddleInitial = string | undefined;
+export type LastName = string;
+export type PersonalName = {
+  firstName: FirstName;
+  middleInitial?: MiddleInitial;
+  lastName: LastName;
+};
+export type EmailAddress = string;
